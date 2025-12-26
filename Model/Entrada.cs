7f82259fa20e166d39192c14c3c80208ba06cf20
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2025-12-26 14:39:33 -0400

    Feat✨:Agrega el modelo de la entidad Entrada

---
+++ Model/Entrada.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gestion_Entrada_Inventario_PA1.Model;
+
+public class Entrada
+{
+    [Key]
+    public int IdEntrada { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "En este campo solo se permiten letras. ")]
+    public string NombreCliente { get; set; }
+
+    [Required(ErrorMessage = "Este campo es obligatorio")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    public decimal Importe { get; set; }
+    public int Cantidad { get; set; }
+    public decimal Precio { get; set; }
+
+
+
+}
+
