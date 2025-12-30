4c19fad87348921a54a897819acf7669aef63519
LEXA-14 <114798705+LEXA-14@users.noreply.github.com>
2025-12-30 17:44:51 -0400

    Feat✨: Agrega los metodos del servicio para Productos

---
+++ Servicess/ProductoServices.cs
@@ -0,0 +1,62 @@
+﻿using Gestion_Entrada_Inventario_PA1.DAL;
+using Gestion_Entrada_Inventario_PA1.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestion_Entrada_Inventario_PA1.Servicess;
+
+public class ProductoServices(IDbContextFactory<Contexto> dbFactory)
+{
+
+    public async Task<bool> Guardar(Producto producto)
+    {
+        if (producto.ProductoId == 0)
+        {
+            producto.Fecha = DateTime.Now;
+            return await Insertar(producto);
+        }
+        else
+        {
+            return await Modificar(producto);
+        }
+
+    }
+
+    private async Task<bool> Insertar(Producto producto)
+    {
+        using var contexto = await dbFactory.CreateDbContextAsync();
+        contexto.Add(producto);
+        return await contexto.SaveChangesAsync() > 0;
+
+    }
+
+    public async Task<bool> Modificar(Producto producto)
+    {
+        using var contexto = await dbFactory.CreateDbContextAsync();
+        contexto.Update(producto);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+    public async Task<bool> Eliminar(int id)
+    {
+        using var contexto = await dbFactory.CreateDbContextAsync();
+
+        var categoria = await contexto.Producto.FirstOrDefaultAsync(c => c.ProductoId == id);
+        if (categoria == null)
+            return false;
+
+        contexto.Producto.Remove(categoria);
+        return await contexto.SaveChangesAsync() > 0;
+    }
+    public async Task<Producto?> Buscar(int id)
+    {
+        using var contexto = await dbFactory.CreateDbContextAsync();
+        return await contexto.Producto
+            .FirstOrDefaultAsync(c => c.ProductoId == id);
+    }
+    public async Task<List<Producto>> Listar()
+    {
+        using var contexto = await dbFactory.CreateDbContextAsync();
+        return await contexto.Producto
+            .OrderBy(c => c.Descripcion)
+            .ToListAsync();
+    }
+}
